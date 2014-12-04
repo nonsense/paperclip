ea679d22ed0d3f852d773b75ac19f0770c57bfcb
Anton Evangelatov <anton.evangelatov@gmail.com>
2014-12-04 10:03:45 +0100

    https expiring_url hack

--- lib/paperclip/storage/fog.rb
+++ lib/paperclip/storage/fog.rb
@@ -141,8 +141,8 @@ module Paperclip
 
       def expiring_url(time = (Time.now + 3600), style_name = default_style)
         time = convert_time(time)
-        if path(style_name) && directory.files.respond_to?(:get_http_url)
-          expiring_url = directory.files.get_http_url(path(style_name), time)
+        if path(style_name) && directory.files.respond_to?(:get_https_url)
+          expiring_url = directory.files.get_https_url(path(style_name), time)
 
           if @options[:fog_host]
             expiring_url.gsub!(/#{host_name_for_directory}/, dynamic_fog_host_for_style(style_name))
